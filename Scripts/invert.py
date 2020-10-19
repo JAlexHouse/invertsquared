9164e5a396cd9ce818e042832f9233befd0d2900
JAlexHouse <james113099@gmail.com>
2020-10-19 12:11:32 -0400

    changed answerlayout, instructions pos

--- Scripts/invert.py
+++ Scripts/invert.py
@@ -91,7 +91,7 @@ class PlayScreen(Screen):
             # generate answer key
             self.generate_answer()
             self.answerlayout.size_hint = [0.3, 0.17]
-            self.answerlayout.pos = (0.74*self.width, 0.82*self.height)  # Not sure where to place this
+            self.answerlayout.pos = (0.35*self.width, 0.7*self.height)  # Not sure where to place this
             self.add_widget(self.answerlayout)
 
             # generate game board
