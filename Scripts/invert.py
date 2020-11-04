0d8a0d2fd74df334da80d4eeac71bb0340718f0e
Chris In <christopherin12@gmail.com>
2020-11-04 09:48:31 -0500

    timer thread works fine, but lose screen has display issues when time runs out

--- Scripts/invert.py
+++ Scripts/invert.py
@@ -13,6 +13,8 @@ from kivy.uix.widget import Widget
 from kivy.core.audio import SoundLoader
 import random
 import os
+import time
+from threading import Thread, Timer
 
 Window.size = (540, 960)
 button_press_sound = SoundLoader.load('../Audio/BUTTON_PRESS.wav')
@@ -78,7 +80,7 @@ class PlayScreen(Screen):
     cols = 3
     moves_made = BoundedNumericProperty(0)
     max_moves = BoundedNumericProperty(15)
-    time_limit_sec = BoundedNumericProperty(30)
+    time_limit_sec = 15
     time_remaining = StringProperty()
     gridlayout = GridLayout(rows=rows, cols=cols)
     answerlayout = GridLayout(rows=rows, cols=cols)
@@ -88,9 +90,10 @@ class PlayScreen(Screen):
     game_tile_sound = None
     filename = os.path.join(dirname, '../Levels/1.txt')
     level = open(filename)
-
+    timer = 0
     def on_enter(self):
         self.set_mode()
+        self.timer = Timer(self.time_limit_sec, self.open_lost)
         if not self.random:
             self.level = open(self.filename)
             self.rows = int(self.level.read(1))
@@ -110,6 +113,13 @@ class PlayScreen(Screen):
 
             self.resume = True
         self.game_tile_sound = SoundLoader.load('../Audio/GAME_TILE_PRESS.wav')
+        if self.game_mode == "Expert":
+            self.timer.start()
+        # start_time = time.time()
+        # elapsed_time = time.time() - start_time
+        # while elapsed_time < self.time_limit_sec:
+        #     elapsed_time = time.time() - start_time
+        #     print(self.time_limit_sec - int(elapsed_time))
         
 
     def generate_grid(self):
@@ -190,12 +200,10 @@ class PlayScreen(Screen):
                         if self.moves_made == self.max_moves:
                             print("Oops, you lost!")
                             self.open_lost()
-                            self.clear_game()
-                            return
-                    return
+                            return 
+                    return 
         print("Yay, you won!")
         self.open_won()
-        self.clear_game()
 
     def reset_board(self):
         self.moves_made = 0
@@ -222,12 +230,16 @@ class PlayScreen(Screen):
         popup.open()
 
     def open_won(self):
+        
         popup = GameWin()
         popup.open()
+        self.clear_game()
 
     def open_lost(self):
+        
         popup = GameLose()
         popup.open()
+        self.clear_game()
 
     def set_mode(self):
         app = App.get_running_app()
@@ -238,6 +250,8 @@ class PlayScreen(Screen):
         else:
             self.random = True
             self.ids.moves.text = "Moves Left: " + str(self.max_moves - self.moves_made)
+    
+    
 
 
 class ScreenManager(ScreenManager):
